085dd942a3aec704b9e3601f6df1a6af1ba89aa4
calebvenable <caleb.venable@dorsetsoftware.com>
2025-12-02 17:12:37 +0000

    Attempt at Day 1 Part 2 (still incorrect)

--- day01/day01.py
+++ day01/day01.py
@@ -1,4 +1,5 @@
 # Advent of Code 2025 - day01
+import math
 import pyperclip
 
 def usefulFunction():
@@ -26,15 +27,47 @@ def partOne(inputFilePath):
 
 # Code for Part Two ----------------------------------------------------
 def partTwo(inputFilePath):
-  return False
+  result = 0
+  dialPosition = 50
+
+  with open(inputFilePath, "r") as file:
+    for line in file:
+      change = 0
+      if (line.startswith("L")):
+        change = int(line.strip('L')) * -1
+      elif (line.startswith("R")):
+        change = int(line.strip('R'))
+
+      if (abs(change) > 99):
+        # Increment result by # of full rotations
+        fullRotations = abs(change) // 100
+        result += fullRotations
+        if (change < 0):
+          change += 100 * fullRotations
+        else:
+          change -= 100 * fullRotations
+        
+        if (change == 0):
+          continue
+
+      dialPosition += change
+      if (dialPosition < 0 or dialPosition > 100):
+        result += 1
+
+      dialPosition %= 100
+
+      if (dialPosition == 0):
+        result += 1
+
+  return result
 
 
 # Run the code for the specified part ----------------------------------
 # answer = partOne("day01/test.txt")
-answer = partOne("day01/input.txt")
+# answer = partOne("day01/input.txt")
 
 # answer = partTwo("day01/test.txt")
-# answer = partTwo("day01/input.txt")
+answer = partTwo("day01/input.txt")
 
 pyperclip.copy(answer)
 print(answer)
