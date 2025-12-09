847344ee7b350de40bfc6757ee44b194239f8b62
calebvenable <caleb.venable@dorsetsoftware.com>
2025-12-09 10:00:50 +0000

    Complete Day 5 Part 2

--- day05/day05.py
+++ day05/day05.py
@@ -2,9 +2,6 @@
 # Run this file with py .\day05\day05.py
 import pyperclip
 
-def usefulFunction():
-  return True
-
 # Input file parsing code to be reused for both parts
 def readFileIntoObject(inputFilePath: str) -> tuple[list[list[int]], list[int]]:
   ranges:list[list[int]] = []
@@ -44,26 +41,33 @@ def partTwo(inputFilePath):
   (freshRanges, _) = readFileIntoObject(inputFilePath)
 
   distinctIds = 0
-  # Compare each range to every range already processed. If it overlaps,
-  # reduce this range. Then count the distinct ids in the reduced range.
-  for i in range(len(freshRanges)):
-    rangeStart = freshRanges[i][0] # Inclusive lower bound
-    rangeEnd = freshRanges[i][1] + 1 # Exclusive upper bound
-    for j in range(i):
-      otherRangeStart = freshRanges[j][0] # Inclusive lower bound
-      otherRangeEnd = freshRanges[j][1] + 1 # Exclusive upper bound
-      
+  smallestRangeStart = min(list(map(lambda r: r[0], freshRanges)))
+  largestRangeEnd = max(list(map(lambda r: r[1], freshRanges)))
 
+  currentId = smallestRangeStart
+  while currentId <= largestRangeEnd:
+    matchedAnyRange = False
+
+    for r in freshRanges:
+      if currentId >= r[0] and currentId <= r[1]:
+        matchedAnyRange = True
+        distinctIds += r[1] - currentId + 1
+        currentId = r[1] + 1
+        break
+
+    if not matchedAnyRange:
+      # Jump up to the next lowest range start
+      currentId = min(list(filter(lambda i: i >= currentId, map(lambda r: r[0], freshRanges))))
 
   return distinctIds
 
 
 # Run the code for the specified part ----------------------------------
 # answer = partOne("day05/test.txt")
-answer = partOne("day05/input.txt")
+# answer = partOne("day05/input.txt")
 
 # answer = partTwo("day05/test.txt")
-# answer = partTwo("day05/input.txt")
+answer = partTwo("day05/input.txt")
 
 pyperclip.copy(answer)
 print(answer)
